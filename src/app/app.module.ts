0b71da704250bd9f4e41d42d1495a3f23b3f9de6
Gheorghii28 <popovici.gheorghii89@gmail.com>
2023-08-14 19:40:48 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ProjectComponent } from './project/project.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormComponent } from './form/form.component';
+import { FooterBarComponent } from './footer-bar/footer-bar.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    MenuComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    PortfolioComponent,
+    ProjectComponent,
+    FooterComponent,
+    FormComponent,
+    FooterBarComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
